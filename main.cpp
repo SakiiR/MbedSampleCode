29de8658d306f0c284433da56a22552efb718d47
SakiiR SakiiR <sakiirlessons@gmail.com>
2018-06-06 00:38:41 +0200

    fixed source

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 
 DigitalOut myled(LED1);
 
-int 
+int
 main() {
-	const int blink_duration = 2;
-	const int blink_interval = 1;
+    const int blink_duration = 2;
+    const int blink_interval = 1;
+
     while(1) {
         myled = 1; // LED is ON
         wait(blink_duration); // 200 ms
